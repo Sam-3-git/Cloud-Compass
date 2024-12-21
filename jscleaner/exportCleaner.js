e24e9d9840eec04020930bdd1d86f7cda7699f45
Sam <75541599+Sam-3-git@users.noreply.github.com>
2024-12-21 15:15:02 -0500

    f

--- jscleaner/exportCleaner.js
+++ jscleaner/exportCleaner.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const jsScriptDir = path.join(__dirname, '../CloudCompass/scripts');
 const files = fs.readdirSync(jsScriptDir);
-if (file.endsWith('.js')) {
+if (files.endsWith('.js')) {
     let content = fs.readFileSync(filePath, 'utf8');
 }
 const exportRegex = /^export\s*\{\};/gm;
